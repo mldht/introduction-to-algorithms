5e728049a8d2c668e95787df020c97d1156d9a6a
Ding Hongtao <charles@china.org>
2018-05-27 17:49:03 +0900

    add weighted activity

--- greedy/weighted_activity.c
+++ greedy/weighted_activity.c
@@ -1,147 +1,83 @@
 #include <stdio.h>
+#define M	100
 
-#define MAX_ACTIVITY_NUM	50
-
-struct activity 
+struct activity
 {
 	int start;
 	int end;
 	int value;
 };
 
-int get_last_compatible_activity(struct activity *list, int activity_index)
+int reconstruction[M][M];
+
+void print_result(int i, int j)
 {
-	int i;
-	for(i = activity_index - 1; i >= 0; i--)
+	if( j - i <= 1 )
 	{
-		if(list[i].end <= list[activity_index].start)
-		{
-			break;
-		}
+		return;
 	}
-
-	return i;
-}
-
-int get_last_compatible_activity2(struct activity *list, int activity_index)
-{
-	int left_index = 0;
-	int right_index = activity_index - 1;
-	while(left_index <= right_index)
+	int k = reconstruction[i][j];
+//	if( k < 0)
+//	{
+//		return ;
+//	}
+	if(k > i && k < j)
 	{
-		if(left_index == right_index)
-		{
-			if(list[right_index].end <= list[activity_index].start)
-			{
-				return right_index;
-			}
-			else
-			{
-				return right_index - 1;
-			}
-		}
-		else
-		{
-			int mid = (left_index + right_index) / 2;
-			if(list[mid].end < list[activity_index].start)
-			{
-				left_index = mid + 1;
-			}
-			else if( list[mid].end > list[activity_index].start)
-			{
-				right_index = mid - 1;
-			}
-			else
-			{
-				return mid;
-			}
-		}
+
+		printf("%d \n", k);
+		print_result(i, k);
+		print_result(k, j);
 	}
 }
 
-
-void get_max_valued_activities( struct activity *list, int size)
+void find_max_valued_activities(struct activity *list, int size, int result[M][M])
 {
-	int values[MAX_ACTIVITY_NUM];
-	int compa_activity_map[MAX_ACTIVITY_NUM];
-	int activity_select_flag[MAX_ACTIVITY_NUM];
-
-	int i;
-	for(i = 0; i < size; i++)
+	int step;
+	for( step = 2; step < size; step++)
 	{
-		values[i] = 0;
-	}
-	compa_activity_map[0] = -1;
-	activity_select_flag[0] = 1;
-
-	values[0] = list[0].value;
-
-//	int j;
-	for(i = 1; i < size; i++)
-	{
-/*		for(j = i - 1; j >= 0; j--)
+		int i, j, k;
+		int tmp = 0;
+		for(i = 0; i < size - step; i++)
 		{
-			if(list[j].end <= list[i].start)
+			j = i + step;
+			for(k = i + 1; k < j; k++)
 			{
-				break;
+				if(list[k].start >=list[i].end && list[k].end <= list[j].start)
+				{
+					int tmp = result[i][k] + result[k][j] + list[k].value;
+					if( tmp > result[i][j] )
+					{
+						result[i][j] = tmp;
+						reconstruction[i][j] = k;
+					}
+				}
 			}
 		}
-*/
-		int compatible_index = get_last_compatible_activity2(list, i);
-
-		compa_activity_map[i] = compatible_index;
-
-		int tmp;
-		if(compatible_index < 0)
-		{
-			tmp = 0;
-		}
-		else
-		{
-			 tmp =values[compatible_index];
-		}
-
-		if( tmp + list[i].value > values[i - 1])
-		{
-			values[i] = tmp + list[i].value;
-			activity_select_flag[i] = 1;
-		}
-		else
-		{	
-			values[i] = values[i-1];
-			activity_select_flag[i] = 0;
-		}
-	}
-	
-	printf("%d\n", values[size -1]);
-
-
-
-	for( i = size-1; i >= 0;)
-	{
-		if(activity_select_flag[i] == 1)
-		{
-			printf("activity: %d, value: %d \n", i, list[i].value);
-			i = compa_activity_map[i];
-		}
-		else
-		{
-			i--;
-		}
-
 	}
 
-	printf("\n");
+	print_result(0, size - 1);
 }
-
 
 int main(void)
 {
-	struct activity list[] = { {1, 4, 3}, {3, 5, 2}, {0, 6, 4}, 
+	struct activity list[] = { {0, 0, 0},
+								{1, 4, 3}, {3, 5, 2}, {0, 6, 4}, 
 								{5, 7, 8}, {3, 9, 2}, {5, 9, 5},
-								{6, 10, 6}, {8, 11, 5},{8, 12, 7},
-								{2, 14, 4}, {12, 16, 5}};
+								{6, 10, 6}, {8, 12, 7}, {12, 16, 5}, 
+								{3000, 3000, 0} };
 
-	get_max_valued_activities(list, 11);
+	int result[M][M];
+	int i, j;
+	for(i = 0; i < M; i++)
+	{
+		for(j = 0; j < M; j++)
+		{
+			result[i][j]  = 0;
+			reconstruction[i][j] = -1;
+		}
+	}
+
+	find_max_valued_activities(list, sizeof(list)/sizeof(list[0]), result);
+	printf("%d\n", result[0][10]);
 	return 0;
 }
